16c2326c0474ffacec8c13b92ad92533513ab599
ryankayne <coope1rk@gmail.com>
2020-01-16 16:36:50 -0500

    Post CRUD works

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const logger = require('./middleware/middleware.js');
 
 const postRouter = require('./posts/postRouter.js');
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
